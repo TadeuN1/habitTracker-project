93afa57b3876cf98f43cd8ad0e382f9152dc9a72
Tadeu Neres <103948326+TadeuN1@users.noreply.github.com>
2026-09-20 17:04:12 -0300

    someChanges

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ const nlwSetup = new NLWSetup(form);
 const button = document.querySelector('header button');
 button.addEventListener('click', add);
 form.addEventListener('change', save);
+const today = new Date().toLocaleDateString('pt-BR', { day: '2-digit', month: '2-digit' });
+
 function add(){
-  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
   const dayExists = nlwSetup.dayExists(today);
   if(dayExists) {
-alert("Dia já incluso 🔴");
-return
-}
-
-  alert('Adicionado com sucesso 🟢');
-nlwSetup.addDay(today);
+    return toggleMessage('Dia já incluso', true);
+  }
+  nlwSetup.addDay(today);
+  toggleMessage('Adicionado com sucesso');
 }
 function save() {
     localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
